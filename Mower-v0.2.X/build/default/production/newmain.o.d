0eac8985dba6b9c77f2497f9f7e01c1738e2a853
Syed Ameen Quadri <ameenquadri2000@gmail.com>
2021-02-18 17:21:01 -0500

    Add files via upload

---
+++ Mower-v0.2.X/build/default/production/newmain.o.d
@@ -0,0 +1,65 @@
+build/default/production/newmain.o.d build/default/production/newmain.o: \
+ newmain.c \
+ c:\program\ files\microchip\xc8\v2.20\avr\lib\gcc\avr\5.4.0\include\xc.h \
+ c:\program\ files\microchip\xc8\v2.20\avr\lib\gcc\avr\5.4.0\include\cci.h \
+ c:\program\ files\microchip\xc8\v2.20\avr\avr\include\avr\io.h \
+ c:\program\ files\microchip\xc8\v2.20\avr\avr\include\avr\sfr_defs.h \
+ c:\program\ files\microchip\xc8\v2.20\avr\avr\include\inttypes.h \
+ c:\program\ files\microchip\xc8\v2.20\avr\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\microchip\xc8\v2.20\avr\avr\include\stdint.h \
+ C:/Program\ Files/Microchip/xc8/v2.20/dfp/xc8/avr/include/avr/iom328pb.h \
+ c:\program\ files\microchip\xc8\v2.20\avr\avr\include\avr\portpins.h \
+ c:\program\ files\microchip\xc8\v2.20\avr\avr\include\avr\common.h \
+ c:\program\ files\microchip\xc8\v2.20\avr\avr\include\avr\version.h \
+ c:\program\ files\microchip\xc8\v2.20\avr\avr\include\avr\fuse.h \
+ c:\program\ files\microchip\xc8\v2.20\avr\avr\include\avr\lock.h \
+ c:\program\ files\microchip\xc8\v2.20\avr\avr\include\stdio.h \
+ c:\program\ files\microchip\xc8\v2.20\avr\lib\gcc\avr\5.4.0\include\stdarg.h \
+ c:\program\ files\microchip\xc8\v2.20\avr\lib\gcc\avr\5.4.0\include\stddef.h \
+ c:\program\ files\microchip\xc8\v2.20\avr\avr\include\stdlib.h \
+ c:\program\ files\microchip\xc8\v2.20\avr\avr\include\util\delay.h \
+ c:\program\ files\microchip\xc8\v2.20\avr\avr\include\util\delay_basic.h \
+ c:\program\ files\microchip\xc8\v2.20\avr\avr\include\math.h \
+ c:\program\ files\microchip\xc8\v2.20\avr\avr\include\string.h
+
+c:\program\ files\microchip\xc8\v2.20\avr\lib\gcc\avr\5.4.0\include\xc.h:
+
+c:\program\ files\microchip\xc8\v2.20\avr\lib\gcc\avr\5.4.0\include\cci.h:
+
+c:\program\ files\microchip\xc8\v2.20\avr\avr\include\avr\io.h:
+
+c:\program\ files\microchip\xc8\v2.20\avr\avr\include\avr\sfr_defs.h:
+
+c:\program\ files\microchip\xc8\v2.20\avr\avr\include\inttypes.h:
+
+c:\program\ files\microchip\xc8\v2.20\avr\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\microchip\xc8\v2.20\avr\avr\include\stdint.h:
+
+C:/Program\ Files/Microchip/xc8/v2.20/dfp/xc8/avr/include/avr/iom328pb.h:
+
+c:\program\ files\microchip\xc8\v2.20\avr\avr\include\avr\portpins.h:
+
+c:\program\ files\microchip\xc8\v2.20\avr\avr\include\avr\common.h:
+
+c:\program\ files\microchip\xc8\v2.20\avr\avr\include\avr\version.h:
+
+c:\program\ files\microchip\xc8\v2.20\avr\avr\include\avr\fuse.h:
+
+c:\program\ files\microchip\xc8\v2.20\avr\avr\include\avr\lock.h:
+
+c:\program\ files\microchip\xc8\v2.20\avr\avr\include\stdio.h:
+
+c:\program\ files\microchip\xc8\v2.20\avr\lib\gcc\avr\5.4.0\include\stdarg.h:
+
+c:\program\ files\microchip\xc8\v2.20\avr\lib\gcc\avr\5.4.0\include\stddef.h:
+
+c:\program\ files\microchip\xc8\v2.20\avr\avr\include\stdlib.h:
+
+c:\program\ files\microchip\xc8\v2.20\avr\avr\include\util\delay.h:
+
+c:\program\ files\microchip\xc8\v2.20\avr\avr\include\util\delay_basic.h:
+
+c:\program\ files\microchip\xc8\v2.20\avr\avr\include\math.h:
+
+c:\program\ files\microchip\xc8\v2.20\avr\avr\include\string.h:
